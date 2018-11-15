daad8f0b75f6514249e1e0413b7310bd5b31e56d
Ryan Losh, O.D <ryan@losh.lan>
2018-11-14 21:36:20 -0600

    Cleaned up the admin site for contact lenses + attributes.  Added link from logo to admin site.  Added scrollbar.

--- cltool/cl/admin.py
+++ cltool/cl/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django import forms
 from .models import ContactAttribute, ContactLens, ContactRebate
+
+class ContactLensAdminForm(forms.ModelForm):
+    class Meta:
+        model = ContactLens
+        fields = '__all__'
+    attributes = forms.ModelMultipleChoiceField(
+        queryset = ContactAttribute.objects.all(),
+        required = False,
+        widget = FilteredSelectMultiple(
+            verbose_name = 'Contact Lens Attributes',
+            is_stacked = False
+        )
+    )
+    def __init__(self, *args, **kwargs):
+        super(ContactLensAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['attributes'].initial = self.instance.contactattribute_set.all()
+    def save(self, commit=True):
+        attribute = super(ContactLensAdminForm, self).save(commit=False)
+        attribute.save()
+        attribute.contactattribute_set.set(self.cleaned_data['attributes'])
+        self.save_m2m()
+        return attribute
 
 # Register your models here.
 class CAInline(admin.TabularInline):
@@ -11,7 +36,7 @@ class CRInline(admin.TabularInline):
     extra = 0
 
 class ContactLensAdmin(admin.ModelAdmin):
-    inlines = [CRInline, CAInline]
+    form = ContactLensAdminForm
 
 admin.site.register(ContactLens, ContactLensAdmin)
 admin.site.register(ContactAttribute)
--- cltool/cl/models.py
+++ cltool/cl/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+import datetime
 # Create your models here.
 class ContactLens(models.Model):
     clens = models.CharField(max_length=128)
@@ -7,12 +8,25 @@ class ContactLens(models.Model):
     cost_per_package = models.DecimalField(max_digits=7, decimal_places=2)
     def __str__(self):
         return self.clens
+    def has_rebate(self):
+        cur_dt = datetime.date.today()
+        rebates = self.contactrebate_set.exclude(to_dt__lt=cur_dt).exclude(from_dt__gt=cur_dt)
+        if len(rebates):
+            return "rebate"
+        else:
+            return ""
+    class Meta:
+        ordering = ["clens"]
+        verbose_name_plural = "Contact lenses"
+
 
 class ContactAttribute(models.Model):
     attribute = models.CharField(max_length=64)
     clenses = models.ManyToManyField(ContactLens)
     def __str__(self):
         return self.attribute
+    class Meta:
+        ordering = ["attribute"]
 
 class ContactRebate(models.Model):
     desc = models.CharField(max_length=64)
@@ -24,3 +38,5 @@ class ContactRebate(models.Model):
     half_amt = models.DecimalField(default=0, max_digits=7, decimal_places=2)
     def __str__(self):
         return self.desc + ': ' + str(self.amt)
+    class Meta:
+        ordering = ["-to_dt"]
